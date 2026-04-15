df52da256c04d67a7e7a2bfd0113e2d2981f66f7
Rishabh <rishiflys@gmail.com>
2026-04-15 23:48:53 +0530

    fix kite version error

--- server.js
+++ server.js
@@ -6,33 +6,27 @@ const { KiteConnect } = require("kiteconnect");
 const app = express();
 app.use(express.json());
 
-// UI
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// Zerodha
 const kite = new KiteConnect({ api_key: process.env.KITE_API_KEY });
 
-let access_token = null;
 let BOT_ACTIVE = true;
 
-// Login
 app.get("/login", (req, res) => res.redirect(kite.getLoginURL()));
 
 app.get("/redirect", async (req, res) => {
   try {
     const session = await kite.generateSession(req.query.request_token, process.env.KITE_API_SECRET);
-    access_token = session.access_token;
-    kite.setAccessToken(access_token);
+    kite.setAccessToken(session.access_token);
     res.send("Login Success ✅");
   } catch {
     res.send("Login Failed ❌");
   }
 });
 
-// Control
 app.get("/start", (req, res) => {
   BOT_ACTIVE = true;
   res.send("🚀 BOT STARTED");
