5ea22ff7eb0a4d3742ec77b8b43edf24f3285a9e
Rishabh <rishiflys@gmail.com>
2026-05-02 03:34:24 +0530

    V66: risk management (position sizing + trade limit + capital protection)

--- engine/strategyEngine.js
+++ engine/strategyEngine.js
@@ -1,25 +1,17 @@
 exports.generateSignals = async function(kc){
-
     const watchlist = ["NSE:TCS","NSE:INFY","NSE:HDFCBANK","NSE:ICICIBANK","NSE:RELIANCE"];
-
     const quotes = await kc.getQuote(watchlist);
 
     let signals = [];
 
     for(let key in quotes){
         const q = quotes[key];
-
         const ltp = q.last_price;
         const open = q.ohlc.open;
         const high = q.ohlc.high;
         const volume = q.volume;
 
-        // SMART FILTERS
-        const bullish = ltp > open;
-        const breakout = ltp > high * 0.995;
-        const volumeSpike = volume > 100000;
-
-        if(bullish && breakout && volumeSpike){
+        if(ltp > open && ltp > high*0.995 && volume > 100000){
             signals.push({
                 symbol: key.replace("NSE:",""),
                 price: parseFloat(ltp.toFixed(2))
@@ -27,5 +19,5 @@ exports.generateSignals = async function(kc){
         }
     }
 
-    return signals.slice(0,3);
+    return signals.slice(0,5);
 };
--- index.js
+++ index.js
@@ -1,43 +1,18 @@
 const express = require("express");
-const { KiteConnect } = require("kiteconnect");
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const kc = new KiteConnect({ api_key: process.env.API_KEY });
-
-app.get("/login", (req,res)=>{
-    res.redirect(kc.getLoginURL());
-});
-
-app.get("/redirect", async (req,res)=>{
-    try{
-        const requestToken = req.query.request_token;
-        const session = await kc.generateSession(requestToken, process.env.API_SECRET);
-
-        const accessToken = session.access_token;
-
-        const forwarded = req.headers['x-forwarded-for'];
-        const realIp = forwarded ? forwarded.split(',')[0] : req.socket.remoteAddress;
-
-        res.send("ACCESS_TOKEN: " + accessToken + "<br>IP: " + realIp);
-
-    }catch(e){
-        res.send(e.message);
-    }
-});
 
 const runLiveEngine = require("./engine/liveEngine");
 
 app.get("/", (req,res)=>{
-    res.send("AlgoBot V65 IP FIX LIVE");
+    res.send("AlgoBot V66 RISK ENGINE LIVE");
 });
 
 app.get("/performance", async (req,res)=>{
     try{
         const capital = 8491.8;
         const activeTrades = await runLiveEngine(capital);
-        res.json({ capital, activeTrades, mode:"SMART_FILTER" });
+        res.json({ capital, activeTrades, mode:"RISK_ENGINE" });
     }catch(e){
         res.json({error:e.message});
     }
