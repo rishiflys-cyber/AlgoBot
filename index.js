639af507091d0b99655a3520efbebbf85c7d3134
Rishabh <rishiflys@gmail.com>
2026-05-03 02:01:16 +0530

    V77 FIX: added /login route + dashboard working + auth flow fixed

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const kc = new KiteConnect({ api_key: process.env.API_KEY });
+
+// LOGIN FIX
+app.get("/login", (req,res)=>{
+    res.redirect(kc.getLoginURL());
+});
+
+app.get("/redirect", async (req,res)=>{
+    try{
+        const requestToken = req.query.request_token;
+        const session = await kc.generateSession(requestToken, process.env.API_SECRET);
+
+        const forwarded = req.headers['x-forwarded-for'];
+        const realIp = forwarded ? forwarded.split(',')[0] : req.socket.remoteAddress;
+
+        res.send("ACCESS_TOKEN: " + session.access_token + "<br>IP: " + realIp);
+
+    }catch(e){
+        res.send(e.message);
+    }
+});
+
 kc.setAccessToken(process.env.ACCESS_TOKEN);
 
-// serve dashboard
+// dashboard
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api/metrics", async (req,res)=>{
@@ -31,4 +52,4 @@ app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname,"public","index.html"));
 });
 
-app.listen(PORT, ()=>console.log("running V77 dashboard"));
+app.listen(PORT, ()=>console.log("running V77 FIX"));
